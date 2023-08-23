41f1f35b39a638f4d660101f68d468f1c948d130
Aecy <devsnake@hotmail.com>
2023-08-23 18:24:08 +0200

    refactor(components): section

--- src/helpers/section.jsx
+++ src/helpers/section.jsx
@@ -2,22 +2,37 @@ import React from "react";
 import {motion} from "framer-motion";
 
 export default function Section ({title}) {
+
+  const fadeInAnimationTitleVariants = {
+    initial: {opacity: 0, y: 100},
+    whileInView: {opacity: 1, y: 0},
+    transition: {duration: 0.4, bounce: 1,}
+  }
+
+  const fadeInAnimationSubtitleVariants = {
+    initial: {opacity: 0, scale: 0},
+    whileInView: {opacity: 0.1, scale: 1},
+    transition: {duration: 0.4, bounce: 1}
+  }
+
   return (
     <React.Fragment>
 
       <motion.h2
-        initial={{opacity: 0, y: 100}}
-        whileInView={{opacity: 1, y: 0}}
-        transition={{duration: 0.4, bounce: 1}}
+        variants={fadeInAnimationTitleVariants}
+        initial="initial"
+        whileInView="whileInView"
+        transition="transition"
         className="section__title"
       >
         {title}
       </motion.h2>
 
       <motion.span
-        initial={{opacity: 0, scale: 0}}
-        whileInView={{opacity: 0.1, scale: 1}}
-        transition={{duration: 0.4, bounce:1}}
+        variants={fadeInAnimationSubtitleVariants}
+        initial="initial"
+        whileInView="whileInView"
+        transition="transition"
         className="section__subtitle"
       >
         {title}
